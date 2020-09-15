04156c560e47af385d5303fa61f9f0299073a063
Joe <joe831011@gmail.com>
2020-09-15 19:11:57 +0800

    add lang translatation of zh-Tw for navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,15 +24,15 @@
           @else
               <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ __('MyProducts') }} <span class="caret"></span>
+                    {{ __('navbar.MyProducts') }} <span class="caret"></span>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{ route('product_lists.index') }}">
-                        {{ __('ProductLists') }}
+                        {{ __('navbar.ProductLists') }}
                     </a>
                     <a class="dropdown-item" href="{{ route('categories.index') }}">
-                      {{ __('Categories') }}
+                      {{ __('navbar.Categories') }}
                   </a>
                 </div>
             </li>
